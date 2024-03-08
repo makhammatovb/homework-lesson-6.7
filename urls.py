d9c018da0d55b47e677f9a7f9e17f2cafbb7fff7
makhammatovb <153611369+makhammatovb@users.noreply.github.com>
2024-03-08 17:48:34 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,26 @@
-from django.urls import path
+"""
+URL configuration for config project.
 
-from .views import index, about, contact, services, staff
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.http import HttpResponse
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-    path('services/', services, name='services'),
-    path('staff/', staff, name='staff'),
+    path('admin/', admin.site.urls),
+    path('', include('app_pages.urls'))
+    # path('', lambda request: HttpResponse('Hello World!'), name='main')
 ]
